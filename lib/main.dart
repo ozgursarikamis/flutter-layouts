39478b7bae44cd2e724c2a27a86aee0e1ec90c0b
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-11-14 04:25:37 +0300

    Brightness, Floating Action Button, persistentFooterButtons

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HelloLayoutsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HelloLayoutsApp extends StatelessWidget {
+  const HelloLayoutsApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      theme: ThemeData(primarySwatch: Colors.pink, brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          centerTitle: false,
+          title: const Text('Building Layouts with Flutter'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text(
+            'Hello Flutter Layouts',
+            style: TextStyle(fontSize: 24),
+          ),
         ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.lightbulb_outline),
+          onPressed: () {
+            print('You rang?');
+          },
+        ),
+        persistentFooterButtons: <Widget>[
+          IconButton(
+              onPressed: () {
+                print('persistent: Add Comment');
+              },
+              icon: const Icon(Icons.add_comment)),
+          IconButton(
+              onPressed: () {
+                print('persistent: Add Alarm');
+              },
+              icon: const Icon(Icons.add_alarm)),
+          IconButton(
+              onPressed: () {
+                print('persistent: Add Location');
+              },
+              icon: const Icon(Icons.add_location))
+        ],
       ),
     );
   }
