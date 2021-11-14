02739124458e517f4d00a49fa3c4ceaf1edd9fee
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-11-14 04:59:42 +0300

    Positioning and Transforming Container

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,17 @@ class MyApp extends StatelessWidget {
       title: 'Your app name',
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: false,
           title: const Text('Your Title here'),
         ),
-        body: const Center(
-          child: Text('Hello World, of course'),
+        body: Container(
+          margin: const EdgeInsets.all(10),
+          width: 200,
+          height: 200,
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(30)),
+              color: Colors.orange,
+              shape: BoxShape.rectangle),
         ),
       ),
     );
