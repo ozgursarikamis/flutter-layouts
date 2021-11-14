504ad4ab6740d5645ddcd6206831f89cf892e2a7
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-11-14 04:48:44 +0300

    resetting the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const HelloLayoutsApp());
-}
+void main() => runApp(const MyApp());
 
-class HelloLayoutsApp extends StatelessWidget {
-  const HelloLayoutsApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.blue,
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-              secondary: const Color(0xffd81b60), brightness: Brightness.dark),
-          textTheme: const TextTheme(
-              bodyText2: TextStyle(
-            fontSize: 24,
-          ))),
       debugShowCheckedModeBanner: false,
+      title: 'Your app name',
       home: Scaffold(
         appBar: AppBar(
-          // centerTitle: false,
-          title: const Text('Building Layouts with Flutter'),
+          title: const Text('Your Title here'),
         ),
         body: const Center(
-          child: Text(
-            'Hello Flutter Layouts',
-          ),
+          child: Text('Hello World, of course'),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.lightbulb_outline),
-          onPressed: () {
-            print('You rang?');
-          },
-        ),
-        persistentFooterButtons: <Widget>[
-          IconButton(
-              onPressed: () {
-                print('persistent: Add Comment');
-              },
-              icon: const Icon(Icons.add_comment)),
-          IconButton(
-              onPressed: () {
-                print('persistent: Add Alarm');
-              },
-              icon: const Icon(Icons.add_alarm)),
-          IconButton(
-              onPressed: () {
-                print('persistent: Add Location');
-              },
-              icon: const Icon(Icons.add_location))
-        ],
       ),
     );
   }
