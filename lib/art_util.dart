8d6bc4f3771f8e126e835c7fef8e3e154d3e0b9d
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-11-29 01:24:33 +0300

    ArtUtils class

---
+++ lib/art_util.dart
@@ -0,0 +1,12 @@
+class ArtUtil {
+  static const caravaggio = '';
+  static const monet = 'Monet';
+  static const vanGogh = 'Van Gogh';
+
+  static const img_caravaggio = 'https://bit.ly/fl_caravaggio';
+  static const img_monet = 'https://bit.ly/fl_monet';
+  static const img_vangogh = 'https://bit.ly/fl_vangogh';
+
+  static const List<String> menuItems = [caravaggio, monet, vanGogh];
+  static const List<String> paths = [img_caravaggio, img_monet, img_vangogh];
+}
