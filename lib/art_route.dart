2d5e49bc923348da43f2614bf69ec4332f28984f
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-11-29 01:24:58 +0300

    Art Route class

---
+++ lib/art_route.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hello_layouts/art_util.dart';
+
+class ArtRoute extends StatelessWidget {
+  final String art;
+  ArtRoute({required this.art});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Navigating art'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            image:
+                DecorationImage(image: NetworkImage(art), fit: BoxFit.cover)),
+      ),
+    );
+  }
+}
