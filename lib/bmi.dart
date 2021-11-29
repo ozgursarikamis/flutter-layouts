a704e14c00e567ac8a38b148d269b0315a1060a8
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-11-29 06:41:45 +0300

    width and height method

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -14,28 +14,45 @@ class _BmiState extends State<Bmi> {
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(middle: Text('BMI')),
       child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Image.network('https://bit.ly/img_bmi'),
-            Util.paddingTop,
-            Row(
-              children: const <Widget>[
-                Text('Height'),
-                Expanded(child: CupertinoTextField())
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints:
+                BoxConstraints(maxHeight: Util.heightOfScreen(context) * .8),
+            child: Column(
+              children: <Widget>[
+                Image.network('https://bit.ly/img_bmi'),
+                Util.paddingTop,
+                Row(
+                  children: <Widget>[
+                    Util.paddingLeft,
+                    SizedBox(
+                      child: const Text('Height'),
+                      width: Util.widthOfScreen(context) / 3,
+                    ),
+                    const Expanded(child: CupertinoTextField()),
+                    Util.paddingLeft
+                  ],
+                ),
+                Util.paddingTop,
+                Row(
+                  children: <Widget>[
+                    Util.paddingLeft,
+                    SizedBox(
+                      child: const Text('Weight'),
+                      width: Util.widthOfScreen(context) / 3,
+                    ),
+                    const Expanded(child: CupertinoTextField()),
+                    Util.paddingLeft
+                  ],
+                ),
+                const Expanded(child: Util.paddingTop),
+                CupertinoButton.filled(
+                    child: const Text('Calculate BMI'),
+                    onPressed: calculateBmi),
+                Util.paddingTop
               ],
             ),
-            Util.paddingTop,
-            Row(
-              children: const <Widget>[
-                Text('Weight'),
-                Expanded(child: CupertinoTextField())
-              ],
-            ),
-            const Expanded(child: Util.paddingTop),
-            CupertinoButton.filled(
-                child: const Text('Calculate BMI'), onPressed: calculateBmi),
-            Util.paddingTop
-          ],
+          ),
         ),
       ),
     );
--- lib/utils.dart
+++ lib/utils.dart
@@ -5,4 +5,12 @@ class Util {
   static const Padding paddingLeft =
       Padding(padding: EdgeInsets.only(left: 12));
   static const TextStyle textStyle = TextStyle(fontSize: 24);
+
+  static double widthOfScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static double heightOfScreen(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
 }
